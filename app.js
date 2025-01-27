2a745575762ac4476a4b7533cf36377d8cd44bfc
SaintoG07 <sainto200507.@gmail.com>
2025-01-27 10:49:00 -0500

    Últimas pulidas para app

--- app.js
+++ app.js
@@ -1,61 +1,58 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Variable dónde vamos almacenar o guardar los nombres ingresados:
+let amigos = [];
 
-//Variables:
-let amigos_agregados = [];
-
-//Función para agregar amigos a la lista con el botón "añadir":
+//Función para agregar los nombres en nuestra lista "amigos" cada vez que el usuario le de al botón "Añadir":
 function agregar_amigo() {
     let amigo_ingresado_por_usuario = document.getElementById('amigo').value;
 
-    //Condicional para virificar que el usuario ingrese un campo de texto válido:
+    //Condicional para verificar que el usuario ingrese un campo de texto válido:
     if (amigo_ingresado_por_usuario) {
-        //luego usando push, insertamos a la lista:
-        amigos_agregados.push(amigo_ingresado_por_usuario);
+        //Si el usuario ingresó un campo de texto(nombre) válido, insertamos a la lista "amigos" con push:
+        amigos.push(amigo_ingresado_por_usuario);
 
-        //llamando a la función para que limpie la UI:
+        //Llamamos la función para limpiar el campo de texto:
         limpiar_UI();
-        console.log(amigos_agregados);
-
-        visualizar_lista(amigos_agregados);
-
+        //Luego con esta función, mostramos en la interfaz los nombres ingresados o los nombres que están
+        //en nuestra lista "amigos"
+        visualizar_lista(amigos, 'listaAmigos');
     }
-    //Y si intenta ingresar un campo de texto vacío, se lanzará una alerta:
+    //En caso de que el usuario intente ingresar un campo de texto vacío, se muestra una alerta:
     else {
-        alert('Por favor, inserte un nombre.')
+        alert('Por favor, inserte un nombre válido.')
     }
 }
 
-//Función para limpiar la UI cada que que el usuario le de a "añadir"
+//Función para limpiar el campo de texto:
 function limpiar_UI() {
     let input_html = document.querySelector('#amigo').value = '';
 }
 
-//Función para eligir un amigo aleatorio de la lista de amigos agregados:
+//Función para elegir o sortear un amigo aleatorio de la lista "amigos" cuando el usuario le de al 
+//botón "Sortear amigo" y luego va mostrar el amigo secreto:
 function sortear_amigo() {
 
-    if (amigos_agregados.length) {
-        //Usando Math.random, escogemos un elemento random de la lista, que en este caso sería el amigo secreto:
-        let amigo_escogifo = Math.floor(Math.random()*(amigos_agregados.length))
+    //Condicional para asegurar que el usuario ingrese nombres antes de sortear:
+    if (amigos.length) {
+        //Usando Math.random, escogemos un elemento(nombre) random de nuestra lista "amigos":
+        let amigo_escogido = Math.floor(Math.random()*(amigos.length))
+        //Luego de escoger el amigo secreto, se muestra:
         let mostrar_resultado = document.getElementById('resultado');
-        mostrar_resultado.innerHTML = amigos_agregados[amigo_escogifo];
+        mostrar_resultado.innerHTML = `El amigo secreto es: ${amigos[amigo_escogido]}`;
     }
+    //En caso de que el usuario intente sortear sin ingresar nombres, se muestra una alerta:
     else {
         alert('Primero ingrese algunos nombres.');
     }
 }
 
 //Función para visualizar la lista de amigos en HTML:
-function visualizar_lista(lista) {
-    let para_viualizar = document.getElementById('listaAmigos');
-    para_viualizar.innerHTML = ''
-
-    //let elemento_lista = '';
+function visualizar_lista(lista, id) {
+    let para_visualizar = document.getElementById(id);
+    para_visualizar.innerHTML = ''
+    
     lista.forEach(element => {
         let elemento_lista = document.createElement('li');
-        //elemento_lista += `<li>${element}</li>`;
         elemento_lista.textContent = element;
-        para_viualizar.appendChild(elemento_lista);
-
-        //ara_viualizar.innerHTML = elemento_lista;
+        para_visualizar.appendChild(elemento_lista);
     });
 }
